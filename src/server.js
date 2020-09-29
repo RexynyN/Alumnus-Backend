0088f770e455836c15b2598899b1981b45f42879
Breno Nogueira <45900808+RexynyN@users.noreply.github.com>
2020-09-29 20:58:05 -0300

    Fix 7.0

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 
 const express = require('express');
 const cors = require('cors');
+const favicon = require('serve-favicon');
+const path = require('path')
 
 const routes = require('./routes');
 
@@ -10,5 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(favicon(path.join(__dirname, 'favicon.ico')));
 
 app.listen(process.env.PORT);
+ 
